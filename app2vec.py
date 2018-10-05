a57cb62ff0a11b560fb13741f24be77146960845
TIS-JOEY <m10509208@gapps.ntust.edu.tw>
2018-10-05 15:37:10 +0800

    add Affinity Propagation Function

--- app2vec.py
+++ app2vec.py
@@ -1,27 +1,37 @@
 from gensim.models import Word2Vec
 import pandas as pd
+import numpy as np
+from sklearn.externals import joblib
+import pickle
+import collections
+
 
 class App2Vec:
-	def __init__(self,stop_app_path):
+	def __init__(self,stop_app_path = []):
 		'''
 		training_data：Store the training data
 		stop_app：Store the stop app. These stop apps won't treat as the training data for App2Vec.
+		label2id：Store the mapping with cluster labels and app sequences.
 		'''
 
 		self.training_data = []
-		with open(stop_app,'r') as f:
-			self.stop_app = f.read().split('\n')
+		self.stop_app = []
+		self.label2id = collections.defaultdict(list)
 
-	# half ignore process mode..
-	def ignore_all_get_app(self,each_row):
-		each_row = each_row.split()
-		res = []
-		for ele in each_row:
-			if ele in self.stop_app:
+		if stop_app_path:
+			with open(stop_app_path,'r') as f:
+				self.stop_app = f.read().split('\n')
+
+	# half ignore mode processing..
+	def ignore_all_get_app(self,each_app_seq):
+		app_seq_list = each_app_seq.split()
+		result = []
+		for app in app_seq_list:
+			if app in self.stop_app:
 				return []
 			else:
-				res.append(ele)
-		return [res]
+				result.append(app)
+		return [result]
 
 	# provide the training data for App2Vec.
 	def csv2training_data(self,raw_file_path,ignore_all = True):
@@ -30,16 +40,19 @@ class App2Vec:
 		ignore_all(Optional)：Ignore mode，True is Full ignore mode，False is half ignore mode.
 		'''
 
-		df = pd.read_csv(file_path,header = None)
+		df = pd.read_csv(raw_file_path,header = None)
 
-		for i,j in df.iterrows():
+		for index,each_app_seq in df.iterrows():
 
+			#Full ignore mode
 			if ignore_all:
-				for each_row in (map(self.ignore_all_get_app,j.tolist())):
-					self.training_data.extend(each_row)
+				for each_app_list in (map(self.ignore_all_get_app, each_app_seq.tolist())):
+					self.training_data.extend(each_app_list)
+			
+			#Half ignore mode
 			else:
-				self.training_data.append([k for ele in j.tolist() for k in ele.split(' ') if k not in stop_app])
-
+				self.training_data.append([app for ele_app_list in each_app_seq.tolist() for app in each_app_list.split(' ') if app not in stop_app])
+		
 			
 	#Train the app2vec.
 	def training_App2Vec(self,app2vec_model_path):
@@ -47,8 +60,8 @@ class App2Vec:
 		app2vec_model_path：The storage location of the app2vec model.
 		'''
 
-		#Views more, go to https://radimrehurek.com/gensim/models/word2vec.html
-		model = Word2Vec(self.training_data,sg=1,size = 128,window = 3,seed = 0,min_count = 0,iter = 10,compute_loss=True)
+		#Views more, https://radimrehurek.com/gensim/models/word2vec.html
+		model = Word2Vec(self.training_data,sg=1,size = 64,window = 3,seed = 0,min_count = 0,iter = 10,compute_loss=True)
 		model.save(app2vec_model_path)
 
 	#Train the ANN
@@ -58,22 +71,22 @@ class App2Vec:
 		num_tree：The number of trees of your ANN forest. More tress more accurate.
 		ann_model_path：The storage path of ANN model.
 		'''
-		
-		#View more, go to https://github.com/spotify/annoy.
+
+		#View more, https://github.com/spotify/annoy.
 		from annoy import AnnoyIndex
 
 		#load app2vec model.
-		model = Word2Vec.load(model_path)
+		app2vec_model = Word2Vec.load(app2vec_model_path)
 
 		#get the vector of app2vec.
-		vector = model.wv.syn0
+		vector = app2vec_model.wv.syn0
 		
 		t = AnnoyIndex(dim)
 		
 		
-		for i in model.wv.vocab:
+		for i in app2vec_model.wv.vocab:
 			#get the mapping id.
-			index = model.wv.vocab[str(i)].index
+			index = app2vec_model.wv.vocab[str(i)].index
 
 			#add the mapping.
 			t.add_item(index,vector[index])
@@ -83,10 +96,45 @@ class App2Vec:
 
 		#save the model
 		t.save(ann_model_path)
+
+	def affinity_propagation(self,app2vec_model_path,af_model_path,prefer):
+		#View more, go to http://scikit-learn.org/stable/modules/generated/sklearn.cluster.AffinityPropagation.html.
+		from sklearn.cluster import AffinityPropagation
+
+		#load app2vec model.
+		app2vec_model = Word2Vec.load(app2vec_model_path)
+
+		#get the vector of app2vec.
+		vector = app2vec_model.wv.syn0
+
+		#store the training data of AF.
+		af_training_data = []
+
+		#average the vector of each app sequence as a unit
+		for app_seq in self.training_data:
+			af_training_data.append(np.mean([app2vec_model[app] for app in app_seq],0))
+
+		# train the af model.
+		af_model = AffinityPropagation(preference = prefer).fit(af_training_data)
+
+		# save the model
+		joblib.dump(af_model, af_model_path)
+
+
+	def get_label2id(self,af_model_path):
+		# load af model
+		af = joblib.load(af_model_path)
+
+		# build a label2id dictionary
+		for index,label in enumerate(af.labels_):
+			self.label2id[label].append(index)
+
 		
 		
 if __name__ == "__main__":
 	app2vec = App2Vec()
-	app2vec.csv2training_data(raw_file_path = '/Users/apple/Documents/raw_data.csv')
+	app2vec.csv2training_data(raw_file_path = '/Users/apple/Documents/paper/userIntentDataset-master/resources/R1.csv')
 	app2vec.training_App2Vec(app2vec_model_path = '/Users/apple/Documents/app2vec.model')
 	app2vec.ANN(dim = 64,num_tree = 10000,app2vec_model_path = '/Users/apple/Documents/app2vec.model',ann_model_path = '/Users/apple/Documents/ANN.model')
+	app2vec.affinity_propagation(app2vec_model_path = '/Users/apple/Documents/app2vec.model',af_model_path = '/Users/apple/Documents//NewAFCluster.pkl',prefer = -30)
+	app2vec.get_label2id(af_model_path = '/Users/apple/Documents//NewAFCluster.pkl')
