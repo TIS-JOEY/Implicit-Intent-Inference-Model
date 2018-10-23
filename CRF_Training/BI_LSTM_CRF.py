555e30a848f8868b4292656b2d96aa1bf09abb96
Tseng, I-Hsiu <43575082+TIS-JOEY@users.noreply.github.com>
2018-10-24 00:53:52 +0800

    Add files via upload

---
+++ CRF_Training/BI_LSTM_CRF.py
@@ -0,0 +1,228 @@
+import pandas as pd
+import numpy as np
+import codecs
+from hanziconv import HanziConv
+import re
+import jieba
+from sklearn.model_selection import train_test_split
+from keras_contrib.layers import CRF
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Model, Input
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+import matplotlib.pyplot as plt
+from keras_contrib.utils import save_load_utils
+import json
+from seqeval.metrics import precision_score, recall_score, f1_score, classification_report
+
+
+class processData:
+	def __init__(self):
+		self.count = 1
+		self.training_data = []
+		self.re_words = re.compile(u"[\u4e00-\u9fa5]+")
+		self.word2id = {}
+		self.id2word = {}
+		self.mapping_count = 1
+		self.tag2id = {'O':0,'COMMENT':1,'PERIOD':2}
+		self.id2tag = {0:'O',1:'COMMENT',2:'PERIOD'}
+
+	def _processAns(self,cut_sen):
+		if(cut_sen != ''):
+			sen = list(jieba.cut(cut_sen))
+			result = []
+			for i in range(len(sen)):
+				if(sen[i]=='，' and result!=[]):
+					result[-1][1] = 'COMMENT'
+				elif(self.re_words.search(sen[i])):
+					if(sen[i] not in self.word2id):
+						self.word2id[sen[i]] = self.mapping_count
+						self.id2word[self.mapping_count] = sen[i]
+						self.mapping_count+=1
+					result.append([sen[i],'O'])
+
+			if(result!=[]):
+				result[-1][1] = 'PERIOD'
+				result = list(map(lambda x:tuple(x),result))
+			else:
+				result = None
+			return result
+
+	def processAns(self):
+		with codecs.open('corpus.txt','r') as f:
+			for i in f.readlines():
+				if(i.startswith(str(self.count))):
+					each_par = HanziConv.toTraditional(i.split('++$++')[1].split()[0])
+					each_lists = each_par.replace('！','。').split('。')
+
+					train_unit = list(map(self._processAns,each_lists))
+
+					if train_unit!=None:		
+						self.training_data.extend(list(filter(lambda x: x != None, train_unit)))
+						self.count+=1
+
+		self.saveParameters()
+
+	def getParameters(self):
+		return self.training_data,self.word2id,self.id2word,self.tag2id,self.id2tag
+
+	def saveParameters(self):
+		with open('CRF_data.txt','w') as f:
+			f.write(json.dumps(self.training_data))
+
+		with open('word2id.txt','w') as f:
+			f.write(json.dumps(self.word2id))
+
+		with open('id2word.txt','w') as f:
+			f.write(json.dumps(self.id2word))
+
+		with open('tag2id.txt','w') as f:
+			f.write(json.dumps(self.tag2id))
+
+		with open('id2tag.txt','w') as f:
+			f.write(json.dumps(self.id2tag))
+
+	def loadParameters(self):
+		with open('CRF_data.txt','r') as f:
+			self.training_data = json.load(f)
+
+		with open('word2id.txt','r') as f:
+			self.word2id = json.load(f)
+
+		with open('id2word.txt','r') as f:
+			self.id2word = json.load(f)
+
+		with open('tag2id.txt','r') as f:
+			self.tag2id = json.load(f)
+
+		with open('id2tag.txt','r') as f:
+			self.id2tag = json.load(f)
+
+
+class BI_LSTM_CRF:
+	def __init__(self,training_data,word2id,id2word,tag2id,id2tag):
+		self.training_data = training_data
+		self.word2id = word2id
+		self.id2word = id2word
+		self.tag2id = tag2id
+		self.id2tag = id2tag
+		self.max_len = 50
+		self.num_word = len(word2id)
+		self.num_tag = len(tag2id)
+		self.X = None
+		self.y = None
+
+	def pad_process(self):
+		X = [[int(self.word2id[w[0]]) for w in s] for s in self.training_data]
+		self.X = pad_sequences(maxlen = self.max_len,sequences = X,padding = 'post',value = 0)
+
+		y = [[int(self.tag2id[w[1]]) for w in s] for s in self.training_data]
+		y = pad_sequences(maxlen = self.max_len,sequences = y,padding = "post",value = self.tag2id['O'])
+		self.y = [to_categorical(i, num_classes = self.num_tag) for i in y]
+
+	def train_test_split(self):
+		return train_test_split(self.X, self.y, test_size=0.1)
+
+	def train(self):
+		input = Input(shape = (self.max_len,))
+		model = Embedding(input_dim = self.num_word + 1, output_dim = 20, input_length = self.max_len, mask_zero = True)(input)
+		model = Dropout(0.1)(model)
+		model = Bidirectional(LSTM(units = 50, return_sequences = True, recurrent_dropout = 0.1))(model)
+		model = TimeDistributed(Dense(50, activation="relu"))(model)  # softmax output layer
+		crf = CRF(self.num_tag)
+		out = crf(model)
+		model = Model(input, out)
+		model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
+
+		self.pad_process()
+		train_X,test_X, train_y, test_y = self.train_test_split()
+
+		history = model.fit(train_X, np.array(train_y), batch_size=32, epochs=1,validation_split=0.1, verbose=1)
+
+		self.make_plot(history)
+		return model
+
+	def evaluate(self,model):
+		self.pad_process()
+		train_X,test_X, train_y, test_y = self.train_test_split()
+
+		test_pred = model.predict(test_X,verbose = 1)
+
+		pred_labels = self._evaluate(test_pred)
+		test_labels = self._evaluate(test_y)
+		print("F1-score: {:.1%}".format(f1_score(test_labels, pred_labels)))
+		print(classification_report(test_labels, pred_labels))
+
+	def _evaluate(self,pred):
+		out = []
+		for pred_i in pred:
+			out_i = []
+			for p in pred_i:
+				p_i = np.argmax(p)
+				out_i.append(self.id2tag[p_i].replace("PAD","O"))
+			out.append(out_i)
+		return out
+
+	def make_plot(self,history):
+		hist = pd.DataFrame(history.history)
+		plt.style.use("ggplot")
+		plt.figure(figsize=(12,12))
+		plt.plot(hist["acc"])
+		plt.plot(hist["val_acc"])
+		plt.show()
+
+	def save_embedding_bilstm2_crf_model(self,model, filename):
+		save_load_utils.save_all_weights(model,filename)
+
+	def get_model(self,filename):
+		model = self.train()
+		save_load_utils.load_all_weights(model, filename)
+		return model
+
+	def load_model(self,filename):
+		input = Input(shape = (self.max_len,))
+		model = Embedding(input_dim = self.num_word + 1, output_dim = 20, input_length = self.max_len, mask_zero = True)(input)
+		model = Dropout(0.1)(model)
+		model = Bidirectional(LSTM(units = 50, return_sequences = True, recurrent_dropout = 0.1))(model)
+		model = TimeDistributed(Dense(50, activation="relu"))(model)  # softmax output layer
+		crf = CRF(self.num_tag)
+		out = crf(model)
+		model = Model(input, out)
+		model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
+
+		save_load_utils.load_all_weights(model, filename,include_optimizer=False)
+		return model
+
+	def predict(self):
+		self.pad_process()
+		train_X,test_X, train_y, test_y = self.train_test_split()
+
+
+		model = self.load_model('BI_LSTM_CRF_model.h5')
+		i = 1927
+		p = model.predict(np.array([test_X[i]]))
+		p = np.argmax(p, axis=-1)
+		true = np.argmax(test_y[i], -1)
+		print("{:15}||{:5}||{}".format("Word", "True", "Pred"))
+		print(30 * "=")
+		for w, t, pred in zip(test_X[i], true, p[0]):
+		    if w != 0:
+		        print("{:15}: {:5} {}".format(self.id2word[w-1], self.id2tag[t], self.id2tag[pred]))
+
+					
+
+if __name__=="__main__":
+	p_data = processData()
+	#p_data.processAns()
+	p_data.loadParameters()
+	training_data,word2id,id2word,tag2id,id2tag = p_data.getParameters()
+	#print([tuple(j) for i in training_data for j in i])
+	#BL_CRF = BI_LSTM_CRF(training_data,word2id,id2word,tag2id,id2tag)
+	#model = BL_CRF.get_model('BI_LSTM_CRF_model.h5')
+	#model = BL_CRF.load_model('BI_LSTM_CRF_model.h5')
+	#BL_CRF.evaluate(model)
+	#BL_CRF.predict()
+
+	#
+	#[[('我','B'),('是','I'),('誰','O')],[('我','B'),('是','I'),('誰','O')]]
+
---
+++ main.py
@@ -0,0 +1,336 @@
+import Training.Model
+import re
+from aip import AipNlp
+import os
+import numpy as np
+from multiprocessing import Process, Manager,Lock
+from collections import Counter
+
+
+def calculateJob(sen,candidate,CanDes,shared_dict):
+	score = calDocScore(sen,CanDes)
+	shared_dict[candidate] = score
+
+def calDocScore(text1,text2):
+	APP_ID = "APP_ID"
+	API_KEY = "APP_KEY"
+	SECRET_KEY = "SECRET_KEY "
+	client = AipNlp(APP_ID,API_KEY,SECRET_KEY)
+	return client.simnet(text1,text2)['score']
+
+class IMIP:
+	def __init__(self,explicit_intent,intentApp,app2vec_model_path,ann_model_path,af_model_path):
+
+		# The mapping between apps and intents
+		self.intentApp = intentApp
+
+		# Store the explicit intents
+		self.explicit_intent = explicit_intent
+
+		# Initial App2Vec class
+		self.app2vec = Model.App2Vec()
+
+		# Initial ANN class
+		self.ann = Model.ANN(app2vec_model_path = app2vec_model_path,ann_model_path = ann_model_path)
+
+		# Initial AF class
+		self.af = Model.AF(app2vec_model_path = app2vec_model_path,af_model_path = af_model_path)
+
+		# Initial BILSTM class
+		self.bilstm = Model.BILSTM(app2vec_model_path = app2vec_model_path,max_len = 5)
+
+		# Initial processData class
+		self.p_data = Model.processData()
+
+		# Set up description
+		self.p_data.processDescription()
+
+		# Load App2Vec model
+		self.app2vec_model = app2vec.load_App2Vec(app2vec_model_path)
+
+	def query(self,input_sen = None,model = 'ANN',doc = True,lstm = True):
+		if model == 'ANN':
+			if lstm:
+				if doc:
+					results = self.BILSTM_ANN_with_doc_process(input_sen)
+				else:
+					results = self.BILSTM_ANN_without_doc_process()
+			else:
+				if doc:
+					results = self.ANN_with_doc_process(input_sen)
+				else:
+					results = self.ANN_without_doc_process()
+
+		else:
+			if lstm:
+				if doc:
+					results = self.BILSTM_AF_with_doc_process(input_sen)
+				else:
+					results = self.BILSTM_AF_without_doc_process()
+			else:
+				if doc:
+					results = self.AF_with_doc_process(input_sen)
+				else:
+					results = self.AF_without_doc_process()
+
+		return results
+
+
+	def ANN_without_doc_process(self):
+
+		# transfer to app
+		apps = [intentApp[i] for i in self.explicit_intent]
+
+		# Load ANN model
+		ann_model = self.ann.load_ANN()
+
+		# Get ids
+		indexs = [self.app2vec_model.wv.vocab[app].index+1 for app in apps]
+
+		# Get their neighbor and flat it to 1D.
+		nbrs = list(itertools.chain.from_iterable([self.ann_model.get_nns_by_item(index,10) for index in indexs]))
+		
+		# Transfer to app and avoid duplicate
+		nbrs = [self.app2vec_model.wv.index2word[nbr-1] for nbr in nbrs if self.app2vec_model.wv.index2word[nbr-1] not in apps]
+
+		counter = collections.Counter(nbrs)
+
+		major_voting_filter = [app_with_count[0] for app_with_count in counter.most_common()]
+
+		# Transfer to class
+		result = self.p_data.checkClass(major_voting_filter,5)
+
+		return result
+
+	def ANN_with_doc_process(self,input_sen):
+
+		# transfer to app
+		apps = [intentApp[i] for i in self.explicit_intent]
+
+		# Load ANN model
+		ann_model = self.ann.load_ANN()
+
+		# Get ids
+		indexs = [self.app2vec_model.wv.vocab[app].index+1 for app in apps]
+
+		# Get their neighbor and flat it to 1D.
+		nbrs = list(itertools.chain.from_iterable([self.ann_model.get_nns_by_item(index,5) for index in indexs]))
+		
+		# Transfer to app and avoid duplicate
+		nbrs = [self.app2vec_model.wv.index2word[nbr-1] for nbr in nbrs if self.app2vec_model.wv.index2word[nbr] not in apps]
+
+		pool = multiprocessing.Pool()
+		manager = Manager()
+
+		# For recording the semantic score
+		shared_dict = manager.dict()
+
+		for nbr_id in range(len(nbr_app)):
+
+			# Calculate the semantic score
+			pool.apply_async(calculateJob,args=(input_sen, nbr_app[nbr_id],self.p_data.app2des[nbr_app[nbr_id]],shared_dict))
+
+		pool.close()
+		pool.join()
+				
+		# Sort by semantic score
+		semantic_filter = sorted(shared_dict,key = shared_dict.get,reverse = True)
+
+		result = self.p_data.checkClass(semantic_filter,5)
+
+		return result
+
+	def AF_without_doc_process(self):
+
+		# Load AF model
+		af_model = self.af.get_af_model()
+
+		# transfer to app
+		apps = [intentApp[i] for i in self.explicit_intent]
+
+		# Get the input vector
+		vector = np.mean([self.app2vec_model[app] for app in apps],0)
+
+		# The predicted label
+		predict_label = af_model.predict([vector])
+
+		# Major voting 
+		counter = collections.Counter(self.af.label2app[predict_label[0]])
+
+		# Choose the top k apps with higher voting and avoid duplicate.
+		major_voting_filter = [app_with_count[0] for app_with_count in counter.most_common() if app_with_count[0] not in apps]
+
+		result = self.p_data.checkClass(major_voting_filter,5)
+		
+	def AF_with_doc_process(self,input_sen):
+
+		# Load AF model
+		af_model = self.af.get_af_model()
+
+		# transfer to app
+		apps = [intentApp[i] for i in self.explicit_intent]
+
+		# Get the input vector
+		vector = np.mean([self.app2vec_model[app] for app in apps],0)
+
+		# The predicted label
+		predict_label = af_model.predict([vector])
+
+		# Get the candididate apps and avoid duplicate
+		candidiates = list(filter(lambda x:x not in apps,list(set(self.af.label2app[predict_label[0]]))))
+
+		pool = multiprocessing.Pool()
+		manager = Manager()
+
+		# For recording the semantic score
+		shared_dict = manager.dict()
+
+		for candidiate_id in range(len(candidiates)):
+
+			# Calculate the semantic score
+			pool.apply_async(calculateJob,args=(input_sen,candidiates[candidiate_id],p_data.app2des[candidiates[candidiate_id]]))
+
+		pool.close()
+		pool.join()
+
+		# Sort by semantic score
+		semantic_filter = sorted(shared_dict,key = shared_dict.get,reverse = True)
+
+		result = self.p_data.checkClass(semantic_filter,5)
+
+		return result
+
+	def BILSTM_ANN_without_doc_process(self,filepath = 'data/BILSTM_model.h5'):
+
+		# Load BILSTM model
+		bilstm_model = self.bilstm.load_BI_LSTM_model(filepath)
+
+		# Load ANN model
+		ann_model = self.ann.load_ANN()
+
+		# transfer to index
+		apps = [self.app2vec_model.wv.vocab[intentApp[i]].index+1 for i in self.explicit_intent]
+
+		# predicted vector
+		vector_predict = self.bilstm.predict(apps,bilstm_model)
+
+		# Get their neighbors.
+		nbrs = ann_model.get_nns_by_vector(vector_predict,10)
+
+		# Transfer them to apps and avoid duplicate
+		nbrs = [self.app2vec_model.wv.index2word[nbr - 1] for nbr in nbrs if self.app2vec_model.wv.index2word[nbr] not in apps]
+
+		counter = collections.Counter(nbrs)
+
+		major_voting_filter = [app_with_count[0] for app_with_count in counter.most_common()]
+
+		result = self.p_data.checkClass(major_voting_filter,5)
+
+	def BILSTM_ANN_with_doc_process(self,input_sen, filepath = 'data/BILSTM_model.h5'):
+
+		# Load BILSTM model
+		bilstm_model = self.bilstm.load_BI_LSTM_model(filepath)
+
+		# Load ANN model
+		ann_model = self.ann.load_ANN()
+
+		# transfer to app
+		apps = [self.app2vec_model.wv.vocab[intentApp[i]].index+1 for i in self.explicit_intent]
+
+		# predicted vector
+		vector_predict = self.bilstm.predict(apps,bilstm_model)
+
+		# Get their neighbor and flat it to 1D.
+		nbrs = ann_model.get_nns_by_vector(vector_predict,len(y_test[app_seq_id]))
+
+		# Transfer to app
+		nbr_app = [self.app2vec_model.wv.index2word[nbr - 1] for nbr in nbrs if self.app2vec_model.wv.index2word[nbr] not in apps]
+
+		pool = multiprocessing.Pool()
+		manager = Manager()
+
+		# For recording the semantic score
+		shared_dict = manager.dict()
+
+		for nbr_id in range(len(nbr_app)):
+
+			# Calculate the semantic score
+			pool.apply_async(calculateJob,args=(input_sen, nbr_app[nbr_id],self.p_data.app2des[nbr_app[nbr_id]],shared_dict))
+
+		pool.close()
+		pool.join()
+				
+		# Sort by semantic score
+		semantic_filter = sorted(shared_dict,key = shared_dict.get,reverse = True)
+
+		result = self.p_data.checkClass(semantic_filter,5)
+
+	def BILSTM_AF_without_doc_process(self,filepath = 'data/BILSTM_model.h5'):
+
+		# Load BILSTM model
+		bilstm_model = self.bilstm.load_BI_LSTM_model(filepath)
+
+		# Load AF model
+		af_model = self.af.get_af_model()
+
+		# transfer to app
+		apps = [self.app2vec_model.wv.vocab[intentApp[i]].index+1 for i in self.explicit_intent]
+
+		# predicted vector
+		vector_predict = self.bilstm.predict(apps,bilstm_model)
+
+		# The predicted label
+		predict_label = af_model.predict([vector_predict])
+
+		# Major voting 
+		counter = collections.Counter(self.af.label2app[predict_label[0]])
+
+		# Choose the top k apps with higher voting and avoid duplicate.
+		major_voting_filter = [app_with_count[0] for app_with_count in counter.most_common() if app_with_count[0] not in apps]
+
+		result = self.p_data.checkClass(major_voting_filter,5)
+
+	def BILSTM_AF_with_doc_process(self,input_sen,filepath = 'data/BILSTM_model.h5'):
+		
+		# Load BILSTM model
+		bilstm_model = self.bilstm.load_BI_LSTM_model(filepath)
+
+		# Load AF model
+		af_model = self.af.get_af_model()
+
+		# transfer to app
+		apps = [self.app2vec_model.wv.vocab[intentApp[i]].index+1 for i in self.explicit_intent]
+
+		# predicted vector
+		vector_predict = self.bilstm.predict(apps,bilstm_model)
+
+		# The predicted label
+		predict_label = af_model.predict([vector_predict])
+
+		# Get the candididate apps and avoid duplicate
+		candidiates = list(filter(lambda x:x not in apps,list(set(self.af.label2app[predict_label[0]]))))
+
+		pool = multiprocessing.Pool()
+		manager = Manager()
+
+		# For recording the semantic score
+		shared_dict = manager.dict()
+
+		for candidiate_id in range(len(candidiates)):
+
+			# Calculate the semantic score
+			pool.apply_async(calculateJob,args=(input_sen,candidiates[candidiate_id],p_data.app2des[candidiates[candidiate_id]]))
+
+		pool.close()
+		pool.join()
+
+		# Sort by semantic score
+		semantic_filter = sorted(shared_dict,key = shared_dict.get,reverse = True)
+
+		result = self.p_data.checkClass(semantic_filter,5)
+
+		return result
+
+
+
+
